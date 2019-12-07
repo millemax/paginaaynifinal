c89049eb4284e32ed42183c337a89cd2d97bfcc7
millermax <romel_23_3@hotmail.com>
2019-12-07 13:03:35 -0500

    funciona el audio

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit ,ViewChild} from '@angular/core';
+import { Component, OnInit ,ViewChild, AfterViewInit} from '@angular/core';
 import * as RecordRTC from 'recordrtc';
 import { DomSanitizer } from '@angular/platform-browser';
+
+//let RecordRTC = require('recordrtc/RecordRTC.min');
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.scss']
 })
-export class ProyectosComponent implements OnInit {
+export class ProyectosComponent{
+
+//video
+private stream: MediaStream;
+private recordRTC: any;
+@ViewChild('video',{static:false}) video;
 
  
 
@@ -23,8 +30,106 @@ export class ProyectosComponent implements OnInit {
 
   constructor(private domSanitizer: DomSanitizer) { }
 
-  ngOnInit() {
+  
+  
+  //codigo de video
+
+  ngAfterViewInit() {
+    // set the initial state of the video
+    let video:HTMLVideoElement = this.video.nativeElement;
+    video.muted = false;
+    video.controls = true;
+    video.autoplay = false;
   }
+
+ 
+
+
+
+
+  toggleControls() {
+    let video: HTMLVideoElement = this.video.nativeElement;
+    video.muted = video.muted;
+    video.controls = video.controls;
+    video.autoplay = !video.autoplay;
+  }
+
+  // controles cuando grabas
+  toggleControlsRecord() {
+    let video: HTMLVideoElement = this.video.nativeElement;
+    video.muted = !video.muted;
+    video.controls = !video.controls;
+    video.autoplay = !video.autoplay;
+  }
+
+  successCallbackVideo(stream: MediaStream) {
+
+    var options = {
+      mimeType: 'video/webm;codecs', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
+      audioBitsPerSecond: 128000,
+      videoBitsPerSecond: 128000,
+      bitsPerSecond: 128000 // if this line is provided, skip above two
+    };
+    this.stream = stream;
+    this.recordRTC = RecordRTC(stream, options);
+    this.recordRTC.startRecording();
+    let video: HTMLVideoElement = this.video.nativeElement;
+    video.srcObject=stream; 
+    //video.src = URL.createObjectURL(stream);
+    
+  
+    this.toggleControlsRecord();
+  }
+
+  errorCallbackVideo() {
+    console.log("no pudimos grabar")
+  }
+
+  processVideo(audioVideoWebMURL) {
+    
+    let video: HTMLVideoElement = this.video.nativeElement;
+    let recordRTC = this.recordRTC;
+    video.src = audioVideoWebMURL;
+    this.toggleControls();
+    let recordedBlob = recordRTC.getBlob();
+    recordRTC.getDataURL(function (dataURL) { });
+  
+  }
+
+  startRecording() {
+    let mediaConstraints = {
+        video:true,
+        audio:true
+    };
+    navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(this.successCallbackVideo.bind(this), this.errorCallbackVideo.bind(this));
+
+
+  }
+
+  stopRecordingVideo() {
+    let recordRTC = this.recordRTC;
+    recordRTC.stopRecording(this.processVideo.bind(this));
+    let stream = this.stream;
+    stream.getAudioTracks().forEach(track => track.stop());
+    stream.getVideoTracks().forEach(track => track.stop());
+  }
+
+  download() {
+    this.recordRTC.save('video.webm');
+  }
+
+
+
+
+
+  //fin de codigo de video
+
+
+
+
+
 
   sanitize(url:string){
     return this.domSanitizer.bypassSecurityTrustUrl(url);
